fdbe6f65986fdebc618be06d58d5b6aee3bac88f
carendeperalta <documentfiles0808@gmail.com>
2025-02-22 18:29:04 +0800

    Exercise #2 - Laravel Architural Concepts

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,7 +2,13 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Http\Request;
+use App\Services\UserService;
+
 abstract class Controller
 {
-    //
+    
+    public function index(UserService $userService) {
+        return $userService->listusers();
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,31 @@
 <?php
 
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Http\Request;
+use App\Services\UserService;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+//service container
+Route::get('/test-container', function (Request $request) {
+    $input = $request->input('key');
+    return $input;
+});
+
+//service provider
+Route::get('/test-provider', function (UserService $userService) {
+    return $userService->listusers();
+    //dd($userService->listusers());
+});
+
+//service user
+Route::get('/test-users',[UserController::class,'index']);
+
+//facade
+Route::get('/test-facade', function (UserService $userService) {
+    return Response::json ($userService->listusers());
+    //dd(Response::json ($userService->listusers()));
 });
